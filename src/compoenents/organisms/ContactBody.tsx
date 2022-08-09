4e1cfceaf661f12b7e26b325d5d1fb6e91fab678
Dauda Adbulganiyu <daudakale@gmail.com>
2022-08-09 16:32:10 +0100

    dont with mobile contact page

--- src/compoenents/organisms/ContactBody.tsx
+++ src/compoenents/organisms/ContactBody.tsx
@@ -7,30 +7,55 @@ import SupportIcon from "../atoms/vectors/contact/SupportIcon";
 
 const ContactBody = () => {
   return (
-    <div className="mx-[9.375rem] flex flex-col shadowContact p-[3.5625rem] mb-[3.5625rem]">
+    <div className="md:mx-[9.375rem] flex flex-col shadowContact p-[3.5625rem] mb-[3.5625rem]">
       <h2 className="text-[2rem] font-semibold leading-[3.36625rem] mb-[2.86375rem]">
         Feel free to drop message for us
       </h2>
-      <div
-        className="grid mb-[2.901875rem]"
-        style={{ gridTemplateColumns: "auto auto" }}
-      >
-        <form action="" className="flex flex-col w-[33.050625rem] gap-y-6 ">
-          <Input placeholder="Name" className="w-full px-2 " type={"text"} />
-          <Input placeholder="Email" className="w-full px-2 " type={"email"} />
-          <Input placeholder="Subject" className="w-full px-2 " type={"text"} />
+      <div className="flex flex-col md:grid md:mb-[2.901875rem] md:grid-cols-[auto_auto]">
+        <form action="" className="flex flex-col md:w-[33.050625rem] gap-y-6 ">
+          <div className="flex flex-col w-full">
+            <span className="text-[#8B8B8B] text-sm leading-[1.425625rem] md:hidden">
+              Full name
+            </span>
+            <Input placeholder="Name" className="w-full px-2 " type={"text"} />
+          </div>
+          <div className="flex flex-col w-full">
+            <span className="text-[#8B8B8B] text-sm leading-[1.425625rem] md:hidden">
+              Subject
+            </span>
+            <Input
+              placeholder="Email"
+              className="w-full px-2 "
+              type={"email"}
+            />
+          </div>
+
+          <div className="flex flex-col w-full">
+            <span className="text-[#8B8B8B] text-sm leading-[1.425625rem] md:hidden">
+              Email
+            </span>
+            <Input
+              placeholder="Subject"
+              className="w-full px-2 "
+              type={"text"}
+            />
+          </div>
+
           <textarea
-            className="w-full px-2 flex-grow "
+            className="w-full px-2 flex-grow h-64 "
             placeholder="Input Text"
           ></textarea>
         </form>
         <div className="flex flex-col gap-y-[2.9rem] ">
-          <div className="flex flex-col bg-primary text-baseText rounded-[0.47411875rem] ">
+          <div className="hidden md:flex flex-col bg-primary text-baseText rounded-[0.47411875rem] ">
             <div
               className="grid ml-[1.8125rem] items-center  mt-[1.754375rem] mb-[1.756875rem] w-[11.5625rem] gap-x-[1.78125rem]"
               style={{ gridTemplateColumns: "1.40375rem auto" }}
             >
-              <EmailSmallIcon className="h-[1.40375rem] w-[1.40375rem]" />
+              <img
+                src="/vectors/envelop.svg"
+                className="h-[1.40375rem] w-[1.40375rem]"
+              />
               <div className="flex flex-col">
                 <span className="text-lg font-semibold leading-[1.36125rem]">
                   info@myjobs.com.ng
@@ -47,7 +72,11 @@ const ContactBody = () => {
               className="grid ml-[1.8125rem] items-center  mt-[1.754375rem] mb-[1.756875rem] gap-x-[1.78125rem]"
               style={{ gridTemplateColumns: "1.40375rem auto" }}
             >
-              <PhoneIcon className="h-[1.40375rem] w-[1.40375rem]" />
+              <img
+                src="/vectors/phone.svg"
+                className="h-[1.40375rem] w-[1.40375rem]"
+              />
+
               <div className="flex flex-col">
                 <span className="text-lg font-semibold leading-[1.36125rem]">
                   +2348135036111
@@ -65,7 +94,10 @@ const ContactBody = () => {
               className="grid ml-[1.8125rem] items-center  mt-[1.754375rem] mb-[1.756875rem] gap-x-[1.78125rem]"
               style={{ gridTemplateColumns: "1.40375rem auto" }}
             >
-              <SupportIcon className="h-[1.40375rem] w-[1.40375rem]" />
+              <img
+                src="/vectors/headset.svg"
+                className="h-[1.40375rem] w-[1.40375rem]"
+              />
               <div className="flex flex-col">
                 <span className="text-sm font-normal leading-[1.05875rem]">
                   lorem ispum lorem
@@ -80,7 +112,7 @@ const ContactBody = () => {
             </div>
           </div>
 
-          <div>
+          <div className="mt-8 md:mt-0">
             <Button
               label="Send"
               className="bg-primary text-baseText w-full text-[1.375rem] font-medium leading-[2.0625rem]"
--- src/compoenents/organisms/ContactTop.tsx
+++ src/compoenents/organisms/ContactTop.tsx
@@ -5,7 +5,10 @@ import LocationIcon from "../atoms/vectors/LocationIcon";
 const ContactTop = () => {
   return (
     <div className=" flex flex-col md:grid md:grid-cols-[auto_auto] md:mx-[9.375rem] md:my-[5.5rem]">
-      <LogoContactSmall className="mx-auto mt-9 md:hidden" />
+      <LogoContactSmall
+        height={"8rem"}
+        className="mx-auto mt-9 md:hidden mb-8"
+      />
       <div className="flex ml-10 md:ml-0 flex-col  gap-y-[1.0625rem] text-primary">
         <span className="font-semibold text-5xl leading-[3.625rem] tracking-[1.5px] ">
           CONTACT US
@@ -16,11 +19,14 @@ const ContactTop = () => {
           speaking to us as easy as possible.
         </span>
       </div>
-      <div className="text-baseText bg-primary md:rounded-[0.625rem] p-6  md:w-[29.125rem] w-full">
+      <div className="text-baseText bg-primary md:rounded-[0.625rem] p-6  md:w-[29.125rem] w-full mb-[5.375rem] md:mb-0 mt-8 md:mt-0 ">
         <span className="3.625 text-2xl font-bold">Where you can find us</span>
         <div className="flex flex-col text-xl font-medium leading-[1.5125rem]">
           <div className="flex mr-[0.6875rem]">
-            <LocationIcon className="w-[3.125rem] h-[3.125rem]" />
+            <img
+              src="/vectors/location.svg"
+              className="w-[3.125rem] h-[3.125rem] mr-5"
+            />
             <span className="mb-[0.9375rem]">
               Lorem Ipsum Lorem Ipsum Lorem
               <br />
